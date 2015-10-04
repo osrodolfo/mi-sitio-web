bbc58c7069bc0009361bf982435ab6f877e04fea
Oscar Estrada <osrodolfo@mesoamericana.edu.gt>
2015-10-03 18:17:05 -0600

    otro error

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.utils import timezone
 from .models import Postear
 # Create your views here.
 def listar_publicaciones(request):
-    publicaciones=Postear.objects.filter(fecha_publicacion_lte=timezone.now()).order_by('fecha_publicacion')
+    publicaciones=Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render (request,'blog/listar_publicaciones',{'publicaciones':publicaciones})
