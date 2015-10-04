f5c0135ee1bf42444ef827418e92066a47aba1f8
Oscar Estrada <osrodolfo@mesoamericana.edu.gt>
2015-10-03 21:37:43 -0600

    errores corregidos

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
-# Create your views here.
+
 def listar_publicaciones(request):
     publicaciones=Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
-    return render (request,'blog/listar_publicaciones',{'publicaciones':publicaciones})
+    return render (request,'blog/listar_publicaciones.html',{'publicaciones':publicaciones})
