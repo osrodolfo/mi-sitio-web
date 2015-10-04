b3d252ba78c3829d1fcce7b6f3a3e6fd28854f4b
Oscar Estrada <osrodolfo@mesoamericana.edu.gt>
2015-10-03 19:16:20 -0600

    Alfin creo que todo esta bien :D

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,4 @@
 from django.contrib import admin
 from .models import Postear
 
-admin.site.register(Postear)
+admin.sitio.register(Postear)
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.listar_publicaciones ),
+    url(r'^$', views.listar_publicaciones),
 ]
