46748920ed45b1afc5db81817559f01d2c3e05c9
Oscar Estrada <osrodolfo@mesoamericana.edu.gt>
2015-10-23 12:37:00 -0600

    Por fin termine mi post, editar y agregar nuevo

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.listar_publicaciones),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detalle_publicar),
+    url(r'^post/new/$', views.postear_nuevo, name='postear_nuevo'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.editar_post, name='editar_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def listar_publicaciones(request):
     publicaciones=Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -10,3 +12,28 @@ def listar_publicaciones(request):
 def detalle_publicar (request,pk):
     publicacion=get_object_or_404(Postear,pk=pk)
     return render(request,'blog/detalle_publicaciones.html',{'publicacion':publicacion})
+
+def postear_nuevo(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('blog.views.detalle_publicar', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/edicion_publicaciones.html', {'form': form})
+
+def editar_post(request, pk):
+    post = get_object_or_404(Postear, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('blog.views.detalle_publicar', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/edicion_publicaciones.html', {'form': form})
